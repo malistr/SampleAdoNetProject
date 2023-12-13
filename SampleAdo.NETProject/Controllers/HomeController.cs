5a277f75d0650950bb74dd61fbb46ade6f9778ee
Mehmet Ali Satar <mehmetalisatarr@gmail.com>
2023-12-14 00:05:31 +0300

    Create ve Update metotları eklendi

--- SampleAdo.NETProject/Controllers/HomeController.cs
+++ SampleAdo.NETProject/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace SampleAdo.NETProject.Controllers
         public IActionResult Index()
         {
             List<Product> products = GetProducts();
+          
             return View(products);
         }
 
@@ -43,6 +44,105 @@ namespace SampleAdo.NETProject.Controllers
                 }
             }
             return products;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            InsertProduct(product);
+            return RedirectToAction("Index","Home");
+        }
+
+        private void InsertProduct(Product product)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                string sql = "Insert into Products(ProductName,UnitPrice) Values(@ProductName,@UnitPrice)";
+                using (SqlCommand sqlCommand = new SqlCommand(sql,sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@ProductName",product.ProductName);
+                    sqlCommand.Parameters.AddWithValue("@UnitPrice", product.UnitPrice);
+
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Product product = GetProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        private Product GetProductById(int id) //public her yerden çağrılabilir, yani her controller'dan public metodlarını çağırabiliriz. private metodlar her yerden çağrılamaz. Bulunduğu controller içerisinden erişilebilir.
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                string sql = "select ProductID,ProductName,UnitPrice from Products where ProductID = @ProductID";
+                using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@ProductID",id);
+
+                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        if (sqlDataReader.Read())
+                        {
+                            Product product = new Product()
+                            {
+                                ProductID = Convert.ToInt32(sqlDataReader["ProductID"]),
+                                ProductName = Convert.ToString(sqlDataReader["ProductName"]),
+                                UnitPrice = Convert.ToDecimal(sqlDataReader["UnitPrice"])
+                            };
+
+                            return product;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            UpdateProduct(product);
+            return RedirectToAction("Index","Home");
+        }
+
+        private void UpdateProduct(Product product)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                string sql = "Update Products Set ProductName = @ProductName, UnitPrice = @UnitPrice where ProductID = @ProductID";
+                using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@ProductID", product.ProductID);
+                    sqlCommand.Parameters.AddWithValue("@ProductName", product.ProductName);
+                    sqlCommand.Parameters.AddWithValue("@UnitPrice", product.UnitPrice);
+
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
